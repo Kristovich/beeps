3425e358cefeed5d7eb7f8b8f1844f7dc74f9803
Kristovich <faceaccount360@gmail.com>
2018-01-23 14:40:25 -0600

    Update mario-victory.sh

--- mario-victory.sh
+++ mario-victory.sh
@@ -1,2 +1,29 @@
 # https://blog.dhampir.no/content/fun-with-beep
-beep -f 130 -l 100 -n -f 262 -l 100 -n -f 330 -l 100 -n -f 392 -l 100 -n -f 523 -l 100 -n -f 660 -l 100 -n -f 784 -l 300 -n -f 660 -l 300 -n -f 146 -l 100 -n -f 262 -l 100 -n -f 311 -l 100 -n -f 415 -l 100 -n -f 523 -l 100 -n -f 622 -l 100 -n -f 831 -l 300 -n -f 622 -l 300 -n -f 155 -l 100 -n -f 294 -l 100 -n -f 349 -l 100 -n -f 466 -l 100 -n -f 588 -l 100 -n -f 699 -l 100 -n -f 933 -l 300 -n -f 933 -l 100 -n -f 933 -l 100 -n -f 933 -l 100 -n -f 1047 -l 400
+beep -p 130  10.0 
+beep -p 262  10.0 
+beep -p 330  10.0 
+beep -p 392  10.0 
+beep -p 523  10.0 
+beep -p 660  10.0 
+beep -p 784  30.0 
+beep -p 660  30.0 
+beep -p 146  10.0 
+beep -p 262  10.0 
+beep -p 311  10.0 
+beep -p 415  10.0 
+beep -p 523  10.0 
+beep -p 622  10.0 
+beep -p 831  30.0 
+beep -p 622  30.0 
+beep -p 155  10.0 
+beep -p 294  10.0 
+beep -p 349  10.0 
+beep -p 466  10.0 
+beep -p 588  10.0 
+beep -p 699  10.0 
+beep -p 933  30.0 
+beep -p 933  10.0 
+beep -p 933  10.0 
+beep -p 933  10.0 
+beep -p 1047  40.0
+
