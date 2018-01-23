b29ea72dd7a8201189a0ed296b0fe76b50dcc8bd
Kristovich <faceaccount360@gmail.com>
2018-01-23 14:43:23 -0600

    Update sandstorm.sh

--- sandstorm.sh
+++ sandstorm.sh
@@ -1,103 +1,102 @@
-#!/bin/bash
+#!/bin/tcsh
 
 # https://github.com/PolishProgrammer/BeepingPenguin
+repeat 4 sleep 0.080 && beep -p 440 4.0
+repeat 1 sleep 0.0160 && beep -p 440 8.0
+repeat 5 sleep 0.080 && beep -p 440 4.0
+repeat 1 sleep 0.0160 && beep -p 440 8.0
+repeat 6 sleep 0.080 && beep -p 587.33 4.0
+repeat 1 sleep 0.0160 && beep -p 587.33 8.0
+repeat 6 sleep 0.080 && beep -p 523.25 4.0
+repeat 1 sleep 0.0160 && beep -p 523.25 8.0
+repeat 1 sleep 0.0160 && beep -p 392 8.0
 
-beep -f 440 -r 4 -l 40 -D 80
-beep -f 440 -r 1 -l 80 -D 160
-beep -f 440 -r 5 -l 40 -D 80
-beep -f 440 -r 1 -l 80 -D 160
-beep -f 587.33 -r 6 -l 40 -D 80 # after 2 beeps tact 2 starts
-beep -f 587.33 -r 1 -l 80 -D 160
-beep -f 523.25 -r 6 -l 40 -D 80
-beep -f 523.25 -r 1 -l 80 -D 160
-beep -f 392 -r 1 -l 80 -D 160
-#End of line 1 tact 3
-beep -f 440 -r 4 -l 40 -D 80
-beep -f 440 -r 1 -l 80 -D 160
-beep -f 440 -r 5 -l 40 -D 80
-beep -f 440 -r 1 -l 80 -D 160
-beep -f 587.33 -r 2 -l 40 -D 80
-#tact 4
+repeat 4 sleep 0.080 && beep -p 440 4.0
+repeat 1 sleep 0.0160 && beep -p 440 8.0
+repeat 5 sleep 0.080 && beep -p 440 4.0
+repeat 1 sleep 0.0160 && beep -p 440 8.0
+repeat 2 sleep 0.080 && beep -p 587.33 4.0
 
-beep -f 440 -r 4 -l 40 -D 80
-beep -f 440 -r 1 -l 80 -D 160
-beep -f 440 -r 5 -l 40 -D 80
-beep -f 440 -r 1 -l 80 -D 160
-beep -f 587.33 -r 2 -l 40 -D 80
-#tact 5
-beep -f 440 -r 2 -l 40 -D 80
-beep -f 440 -r 2 -l 80 -D 160
-beep -f 440 -r 4 -l 40 -D 80
-beep -f 440 -r 1 -l 80 -D 160
-beep -f 523.25 -r 1 -l 160 -D 320
-#t 6
-beep -f 440 -r 2 -l 40 -D 80
-beep -f 440 -r 2 -l 80 -D 160
-beep -f 440 -r 4 -l 40 -D 80
-beep -f 440 -r 1 -l 80 -D 160
-beep -f 523.25 -r 2 -l 80 -D 160
-#t 7
-beep -f 440 -r 2 -l 40 -D 80
-beep -f 440 -r 2 -l 80 -D 160
-beep -f 440 -r 4 -l 40 -D 80
-beep -f 440 -r 2 -l 80 -D 160
-beep -f 587.33 -r 4 -l 40 -D 80 #after 2 beeps tact 7 ends. Tact 8
-beep -f 587.33 -r 2 -l 80 -D 160
 
-beep -f 523.25 -r 4 -l 40 -D 80 #after 2 beeps tact 7 ends. Tact 8
-beep -f 523.25 -r 2 -l 80 -D 160
-beep -f 392 -r 1 -l 80 -D 160
-#t 9 - same as t6
-beep -f 440 -r 2 -l 40 -D 80
-beep -f 440 -r 2 -l 80 -D 160
-beep -f 440 -r 4 -l 40 -D 80
-beep -f 440 -r 1 -l 80 -D 160
-beep -f 523.25 -r 2 -l 80 -D 160
-#t 10 - same as t 6
-beep -f 440 -r 2 -l 40 -D 80
-beep -f 440 -r 2 -l 80 -D 160
-beep -f 440 -r 4 -l 40 -D 80
-beep -f 440 -r 1 -l 80 -D 160
-beep -f 523.25 -r 2 -l 80 -D 160
-#t 11
-beep -f 440 -r 2 -l 40 -D 80
-beep -f 440 -r 1 -l 80 -D 160
-beep -f 523.25 -r 2 -l 80 -D 160 #next note formation same
-beep -f 440 -r 2 -l 40 -D 80
-beep -f 440 -r 1 -l 80 -D 160
-beep -f 523.25 -r 2 -l 80 -D 160 #next note formation same
-#t 12
-beep -f 440 -r 2 -l 40 -D 80
-beep -f 440 -r 1 -l 80 -D 160
-beep -f 523.25 -r 2 -l 80 -D 160 #next note formation same
-beep -f 440 -r 2 -l 40 -D 80
-beep -f 440 -r 1 -l 80 -D 160
-beep -f 523.25 -r 2 -l 80 -D 160
-#t 13 
-beep -f 440 -r 2 -l 40 -D 80
-beep -f 523.25 -r 1 -l 80 -D 160
+repeat 4 sleep 0.080 && beep -p 440 4.0
+repeat 1 sleep 0.0160 && beep -p 440 8.0
+repeat 5 sleep 0.080 && beep -p 440 4.0
+repeat 1 sleep 0.0160 && beep -p 440 8.0
+repeat 2 sleep 0.080 && beep -p 587.33 4.0
 
-beep -f 440 -r 2 -l 40 -D 80
-beep -f 523.25 -r 1 -l 80 -D 160
+repeat 2 sleep 0.080 && beep -p 440 4.0
+repeat 2 sleep 0.0160 && beep -p 440 8.0
+repeat 4 sleep 0.080 && beep -p 440 4.0
+repeat 1 sleep 0.0160 && beep -p 440 8.0
+repeat 1 sleep 0.0320 && beep -p 523.25 1.60
 
-beep -f 440 -r 2 -l 40 -D 80
-beep -f 523.25 -r 1 -l 80 -D 160
+repeat 2 sleep 0.080 && beep -p 440 4.0
+repeat 2 sleep 0.0160 && beep -p 440 8.0
+repeat 4 sleep 0.080 && beep -p 440 4.0
+repeat 1 sleep 0.0160 && beep -p 440 8.0
+repeat 2 sleep 0.0160 && beep -p 523.25 8.0
 
-beep -f 440 -r 2 -l 40 -D 80
-beep -f 523.25 -r 1 -l 80 -D 160
-#t 14
-beep -f 440 -r 2 -l 40 -D 80
-beep -f 523.25 -r 1 -l 80 -D 160
+repeat 2 sleep 0.080 && beep -p 440 4.0
+repeat 2 sleep 0.0160 && beep -p 440 8.0
+repeat 4 sleep 0.080 && beep -p 440 4.0
+repeat 2 sleep 0.0160 && beep -p 440 8.0
+repeat 4 sleep 0.080 && beep -p 587.33 4.0
+repeat 2 sleep 0.0160 && beep -p 587.33 8.0
 
-beep -f 440 -r 2 -l 40 -D 80
-beep -f 523.25 -r 1 -l 80 -D 160
+repeat 4 sleep 0.080 && beep -p 523.25 4.0
+repeat 2 sleep 0.0160 && beep -p 523.25 8.0
+repeat 1 sleep 0.0160 && beep -p 392 8.0
 
-beep -f 440 -r 2 -l 40 -D 80
-beep -f 523.25 -r 1 -l 80 -D 160
+repeat 2 sleep 0.080 && beep -p 440 4.0
+repeat 2 sleep 0.0160 && beep -p 440 8.0
+repeat 4 sleep 0.080 && beep -p 440 4.0
+repeat 1 sleep 0.0160 && beep -p 440 8.0
+repeat 2 sleep 0.0160 && beep -p 523.25 8.0
 
-beep -f 440 -r 2 -l 40 -D 80
-beep -f 523.25 -r 1 -l 80 -D 160
+repeat 2 sleep 0.080 && beep -p 440 4.0
+repeat 2 sleep 0.0160 && beep -p 440 8.0
+repeat 4 sleep 0.080 && beep -p 440 4.0
+repeat 1 sleep 0.0160 && beep -p 440 8.0
+repeat 2 sleep 0.0160 && beep -p 523.25 8.0
 
-#t 15 - 18, final BEEP
-beep -f 587.33 -r 4 -l 640 -D 10
+repeat 2 sleep 0.080 && beep -p 440 4.0
+repeat 1 sleep 0.0160 && beep -p 440 8.0
+repeat 2 sleep 0.0160 && beep -p 523.25 8.0
+repeat 2 sleep 0.080 && beep -p 440 4.0
+repeat 1 sleep 0.0160 && beep -p 440 8.0
+repeat 2 sleep 0.0160 && beep -p 523.25 8.0
+
+repeat 2 sleep 0.080 && beep -p 440 4.0
+repeat 1 sleep 0.0160 && beep -p 440 8.0
+repeat 2 sleep 0.0160 && beep -p 523.25 8.0
+repeat 2 sleep 0.080 && beep -p 440 4.0
+repeat 1 sleep 0.0160 && beep -p 440 8.0
+repeat 2 sleep 0.0160 && beep -p 523.25 8.0
+
+repeat 2 sleep 0.080 && beep -p 440 4.0
+repeat 1 sleep 0.0160 && beep -p 523.25 8.0
+
+repeat 2 sleep 0.080 && beep -p 440 4.0
+repeat 1 sleep 0.0160 && beep -p 523.25 8.0
+
+repeat 2 sleep 0.080 && beep -p 440 4.0
+repeat 1 sleep 0.0160 && beep -p 523.25 8.0
+
+repeat 2 sleep 0.080 && beep -p 440 4.0
+repeat 1 sleep 0.0160 && beep -p 523.25 8.0
+
+repeat 2 sleep 0.080 && beep -p 440 4.0
+repeat 1 sleep 0.0160 && beep -p 523.25 8.0
+
+repeat 2 sleep 0.080 && beep -p 440 4.0
+repeat 1 sleep 0.0160 && beep -p 523.25 8.0
+
+repeat 2 sleep 0.080 && beep -p 440 4.0
+repeat 1 sleep 0.0160 && beep -p 523.25 8.0
+
+repeat 2 sleep 0.080 && beep -p 440 4.0
+repeat 1 sleep 0.0160 && beep -p 523.25 8.0
+
+
+repeat 4 sleep 0.010 && beep -p 587.33 6.40
 
